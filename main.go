052344c1bcc73dc3532a8e3a620b08fea2f35c2e
ahmed-abdelhamid <slevin.glevra@gmail.com>
2026-04-07 04:51:36 +0300

    feat: implement REPL with bufio.Scanner for user input

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+		first := words[0]
+		fmt.Println("Your command was:", first)
+	}
 }
-
